7671cc5958b6c6e43ed64d36dc5f88b69f0fdc8a
vipuls11 <vipulvishwakarma161@gmail.com>
2024-08-17 20:26:43 +0530

    sidedrawer

--- src/SideDrawer/SideDrawer.jsx
+++ src/SideDrawer/SideDrawer.jsx
@@ -3,13 +3,13 @@ import './SideDrawer.css'
 import ReactDOM from 'react-dom'
 import { CSSTransition } from 'react-transition-group';
 
-const SideDrawer = (props, { children, show }) => {
+const SideDrawer = (props) => {
 
     console.log(props, "props", props.show)
 
     const sidedrawer = (<CSSTransition in={props.show} timeout={200} classNames="slide-in-left" mountOnEnter unmountOnExit>
         <aside className="Sidebar" onClick={props.onClick}>
-            {children}
+            {props.children}
             {/* <Compo /> */}
         </aside>
     </CSSTransition>);
